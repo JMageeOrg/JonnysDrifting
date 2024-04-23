4d0d5e7d6e4d0695336bd26f225f332c7c7293fa
jmagee <jmagee@paloaltonetworks.com>
2024-04-23 17:46:27 +0100

    drift

--- s3.tf
+++ s3.tf
@@ -6,4 +6,8 @@ provider "aws" {
 
 resource "aws_s3_bucket" "jmdrift-bucket" {
   bucket = "jm-this-bucket-should-drift"
+  tags = {
+    yor_name  = "jmdrift-bucket"
+    yor_trace = "c5696553-d10c-4c33-955f-2a5265fcaee9"
+  }
 }
