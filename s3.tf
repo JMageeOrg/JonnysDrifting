4efdfc471bed89c5431672e1e35f9f6255fa6858
jmagee <jmagee@paloaltonetworks.com>
2024-04-23 14:23:18 +0100

    IACdrift

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.2"
+  tags = {
+    yor_name  = "s3-bucket"
+    yor_trace = "ad2f5f30-f1eb-4b18-8523-61d538082c4b"
+  }
 }
