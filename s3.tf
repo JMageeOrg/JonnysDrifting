28600abaf1b074e9eacaeef44e97caff94dccce2
jmagee <jmagee@paloaltonetworks.com>
2024-09-16 13:14:36 +0100

    add AWS S3 bucket  resources via Terraform for drift demo

--- s3.tf
+++ s3.tf
@@ -7,4 +7,8 @@ provider "aws" {
 resource "aws_s3_bucket" "jonnysdrifting" {
   bucket = "jm-this-AWSbucket-has-drifted"
 
+  tags = {
+    yor_name  = "jonnysdrifting"
+    yor_trace = "00ceb66a-8823-4706-bd07-5419a1c761b6"
+  }
 }
