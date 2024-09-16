6053ac221e5d383bd991cd7dec1981c5e80182dc
Jonathan Magee <120496483+jmagee70@users.noreply.github.com>
2024-09-16 09:29:42 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,8 +6,5 @@ provider "aws" {
 
 resource "aws_s3_bucket" "jmdrift-bucket" {
   bucket = "jm-this-bucket-should-drift"
-  tags = {
-    yor_name  = "jmdrift-bucket"
-    yor_trace = "c5696553-d10c-4c33-955f-2a5265fcaee9"
-  }
+
 }
