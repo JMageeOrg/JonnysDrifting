8662c6f5f85905502b531a3ce9358d1b9a4f683f
jmagee <jmagee@paloaltonetworks.com>
2024-09-16 09:56:11 +0100

    latest drift update

--- s3.tf
+++ s3.tf
@@ -1,14 +1,10 @@
 provider "aws" {
-  region     = "eu-west-1"
-  access_key = "AXXXXXXXXXXXXXXX"
-  secret_key = "XXXXXXXXXXXXXXX"
+  region     = "us-west-2"
+  access_key = "ASIAUFASNETJTTI5N4PE"
+  secret_key = "gQ6gquU+GIxViHN/MS2bZzu3SNU+wrIgfr9icjc3"
 }
 
-resource "aws_s3_bucket" "jmdrift-bucket" {
-  bucket = "jm-this-bucket-should-drift"
+resource "aws_s3_bucket" "jonnysdrifting" {
+  bucket = "jm-this-AWSbucket-has-drifted"
 
-  tags = {
-    yor_name  = "jmdrift-bucket"
-    yor_trace = "f5f0b6ab-518e-4d58-9dd9-7a07ac3a672a"
-  }
 }
